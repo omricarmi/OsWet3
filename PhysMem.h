2dba06a3f17d2be90a1cc925be22e7aa9451c69d
Omri Carmi <omricarmi@gmail.com>
2018-01-05 10:27:49 +0200

    first commit

---
+++ PhysMem.h
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+#define PHYSMEMSZ 262144
+
+class PhysMem {
+friend class VirtualMemory;
+public:
+	static PhysMem& Access();
+	private:
+		PhysMem() {}
+		static int* physMem;
+
+	public:
+		PhysMem(PhysMem const&) = delete;
+		void operator=(PhysMem const&) = delete;
+		int* GetFrame(int frameNumber);
+};
